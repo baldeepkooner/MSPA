476f99ba8302c36d70b181229c93a9e7d8b12ac8
Baldeep Kooner <baldeepkooner@cmail.carleton.ca>
2019-03-06 15:20:26 -0500

    First commit

---
+++ examples/basic_fullvector_MaterialChanges.m
@@ -0,0 +1,61 @@
+% This example shows how to calculate and plot both the
+% fundamental TE and TM eigenmodes of an example 3-layer ridge
+% waveguide using the full-vector eigenmode solver.  
+
+% Refractive indices:
+n1 = 3.34;          % Lower cladding
+n2 = 3.44;          % Core
+n3 = 1.00;          % Upper cladding (air)
+
+% Layer heights:
+h1 = 2.0;           % Lower cladding
+h2 = 1.3;           % Core thickness
+h3 = 0.5;           % Upper cladding
+
+% Horizontal dimensions:
+rh = 1.1;           % Ridge height
+rw = 1.0;           % Ridge half-width
+side = 1.5;         % Space on side
+
+% Grid size:
+dx = 0.0325;        % grid size (horizontal)
+dy = 0.0325;        % grid size (vertical)
+
+lambda = 1.55;      % vacuum wavelength
+nmodes = 1;         % number of modes to compute
+neffArr = zeros(10, 1);
+cnt = 1; 
+nArr = 3.305:0.0135:3.44;
+
+for n2 = 3.305:0.0135:3.44
+    [x,y,xc,yc,nx,ny,eps,edges] = waveguidemesh([n1,n2,n3],[h1,h2,h3], ...
+                                            rh,rw,side,dx,dy); 
+
+    % First consider the fundamental TE mode:
+
+    [Hx,Hy,neff] = wgmodes(lambda,n2,nmodes,dx,dy,eps,'000A');
+    neffArr(cnt) = neff; 
+    cnt = cnt + 1;
+    fprintf(1,'neff = %.6f\n',neff);
+
+
+    figure(1);
+    subplot(121);
+    contourmode(x,y,Hx);
+    title('Hx (TE mode)'); xlabel('x'); ylabel('y'); 
+    for v = edges, line(v{:}); end
+
+    subplot(122);
+    contourmode(x,y,Hy);
+    title('Hy (TE mode)'); xlabel('x'); ylabel('y'); 
+    for v = edges, line(v{:}); end
+end
+figure(2)
+comet(nArr, neffArr)
+title('Neff vs. ridge index')
+
+% The behaviour of the H field (both Hx and Hy) follows a similar 
+% behaviour pattern to when the geometry was changed to vary the ridge 
+% half-width 
+% As the index drops, the Hx fields transfers to the base of the ridge
+% while the Hy field is stretched across a larger area
